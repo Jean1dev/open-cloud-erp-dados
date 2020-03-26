40febbf78de610bac307bc8e4c0fe91bf9b0fff9
Jean <jeanlucafp@gmail.com>
2020-03-26 00:03:43 -0300

    Criação e configuração do projeto

---
+++ back-end/cursofullstackmoderno/src/main/java/com/curso/fullstack/moderno/cursofullstackmoderno/CursofullstackmodernoApplication.java
@@ -0,0 +1,13 @@
+package com.curso.fullstack.moderno.cursofullstackmoderno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursofullstackmodernoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursofullstackmodernoApplication.class, args);
+	}
+
+}
---
+++ back-end/cursofullstackmoderno/src/test/java/com/curso/fullstack/moderno/cursofullstackmoderno/CursofullstackmodernoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.fullstack.moderno.cursofullstackmoderno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursofullstackmodernoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
