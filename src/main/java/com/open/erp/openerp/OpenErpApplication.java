6962d229f3a8ce15cc9d1edaec6c690949104ac3
Jean <jeanluca.fernandes@totvs.com.br>
2020-12-16 21:54:29 -0200

    other changes

---
+++ src/main/java/com/open/erp/openerp/OpenErpApplication.java
@@ -0,0 +1,13 @@
+package com.open.erp.openerp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenErpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenErpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/open/erp/openerp/OpenErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.open.erp.openerp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
