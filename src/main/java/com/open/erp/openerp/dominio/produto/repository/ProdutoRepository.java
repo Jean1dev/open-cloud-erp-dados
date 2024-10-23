1c781a4d5113f63ff41a7f9566b3b30e23f345c0
Jeanluca <jeanlucafp@gmail.com>
2024-10-23 14:10:11 -0300

    v1/filtro de produtos

--- src/main/java/com/open/erp/openerp/dominio/produto/repository/ProdutoRepository.java
+++ src/main/java/com/open/erp/openerp/dominio/produto/repository/ProdutoRepository.java
@@ -1,8 +1,11 @@
 package com.open.erp.openerp.dominio.produto.repository;
 
 import com.open.erp.openerp.dominio.produto.model.Produto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface ProdutoRepository extends MongoRepository<Produto, String>, PagingAndSortingRepository<Produto, String> {
+    Page<Produto> findByNomeContaining(String nome, PageRequest of);
 }
--- src/main/java/com/open/erp/openerp/dominio/produto/service/ProdutoService.java
+++ src/main/java/com/open/erp/openerp/dominio/produto/service/ProdutoService.java
@@ -3,6 +3,8 @@ package com.open.erp.openerp.dominio.produto.service;
 import com.open.erp.openerp.dominio.produto.model.Produto;
 import com.open.erp.openerp.dominio.produto.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -17,5 +19,12 @@ public class ProdutoService {
         Optional<Produto> optionalProduto = repository.findById(idProduto);
 
         return optionalProduto.isPresent() ? optionalProduto.get().getNome() : "Não encontrado";
+    }
+
+    public Page<Produto> peformSearch(String nome, Integer limit, Integer offset) {
+        if (nome == null || nome.isEmpty()) {
+            return repository.findAll(PageRequest.of(offset, limit));
+        }
+        return repository.findByNomeContaining(nome, PageRequest.of(offset, limit));
     }
 }
